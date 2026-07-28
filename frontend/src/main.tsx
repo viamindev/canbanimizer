90e447ba1a924e0339fdbaff3b065a701ee3650c
viamind <viamindev@proton.me>
2026-07-28 06:49:46 +0300

    Initialize app with context provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { App } from './App'
+import { AppProvider } from './app/provider'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AppProvider>
+      <App />
+    </AppProvider>
   </StrictMode>,
 )
